c6fb1998d99a17f62d5427a14b0edcb68c24fadf
GuillemotHugo <hugo221204.guillemot@gmail.com>
2023-11-15 09:12:40 +0100

    Commit2

---
+++ exo2/exo2.js
@@ -0,0 +1,7 @@
+var MavariableString="toto";
+var MavariableNumerique=100;
+var Mavariabletableau=['Apple','Banana'];
+var MavariableObjet= window
+
+alert(MavariableString + MavariableNumerique + Mavariabletableau[0] + 
+ MavariableObjet.location.pathname)
